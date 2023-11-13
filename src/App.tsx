2294688507e3c2ffd1e130c1e038c16316712e2a
GONZALO <CMG_DEV@hotmail.com>
2023-11-13 09:58:18 -0300

    gonzalo.

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
       <div>Total Memory: {memoryInfo.totalMemory}</div>
       <div>Free Memory: {memoryInfo.freeMemory}</div>
       <div className="flex absolute bottom-0 bg-white w-10 h-5">
-        <p>Gonzalo12dddd3ads</p>
+        <p>Gonssszalo12dddd3ads</p>
       </div>
     </div>
   );
