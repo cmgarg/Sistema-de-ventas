071becfe50c0801004fa78cac306799d3c701d57
GONZALO <CMG_DEV@hotmail.com>
2023-11-13 10:30:50 -0300

    Ahora el manejo de ventana funcionaaa

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="w-screen h-screen bg-gray-900">
       <Header />
+
       <div className="flex absolute bottom-0 bg-white w-10 h-5"></div>
     </div>
   );
