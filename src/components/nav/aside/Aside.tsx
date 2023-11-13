9c5143b716531c74eeff6a00ef7e1af72c13eb68
GONZALO <CMG_DEV@hotmail.com>
2023-11-13 19:55:21 -0300

    cmg

--- src/components/nav/aside/Aside.tsx
+++ src/components/nav/aside/Aside.tsx
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function Aside() {
-  return <div className="flex flex-1 w-10 bg-stone-700">asd</div>;
+  return <div className="flex flex-1 w-10 bg-stone-700">
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-list" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+    </svg>
+    </div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>;
 }
