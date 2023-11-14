ca7dc88b87348f1d434f8c7a92c3955c5c25814e
GONZALO <CMG_DEV@hotmail.com>
2023-11-14 11:50:24 -0300

    sseee

--- src/components/nav/aside/Aside.tsx
+++ src/components/nav/aside/Aside.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Menu from "../../../assets/asidesvg/Menu.jsx";
 import Usuario from "../../../assets/asidesvg/Usuario.jsx";
 import Articulos from "../../../assets/asidesvg/Articulos.jsx";
@@ -11,43 +11,52 @@ import Estadisticas from "../../../assets/asidesvg/Estadisticas.jsx";
 import Configuracion from "../../../assets/asidesvg/Configuracion.jsx";
 
 export default function Aside() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
+
   return (
     <div className=" flex flex-col w-14 h-full items-center justify-stretch bg-gray-800 border-r-2 border-zinc-950">
       <div className=" flex flex-col h-full justify-between ">
         <div>
-      <div className=" flex items-center justify-center w-10 h-10 mt-5 mb-5">
-        <Menu></Menu>
-      </div>
-      <div className=" flex items-center justify-center w-10 h-10 mt-3 mb-3">
-        <Usuario></Usuario>
-      </div>
-      <div className=" flex items-center justify-center w-10 h-10 mt-3 mb-3">
-        <Articulos></Articulos>
-      </div>
-      <div className=" flex items-center justify-center w-10 h-10 mt-3 mb-3">
-        <Ventas></Ventas>
-      </div>
-      <div className=" flex items-center justify-center w-10 h-10 mt-3 mb-3">
-        <Navegador></Navegador>
-      </div>
-      <div className=" flex items-center justify-center w-10 h-10 mt-3 mb-3">
-        <Stock></Stock>
-      </div>
-      <div className=" flex items-center justify-center w-10 h-10 mt-3 mb-3">
-        <Caja></Caja>
-      </div>
-      <div className=" flex items-center justify-center w-10 h-10 mt-3 mb-3">
-        <Cuentas></Cuentas>
-      </div>
-      <div className=" flex items-center justify-center w-10 h-10 mt-3 mb-3">
-        <Estadisticas></Estadisticas>
-      </div>
-      </div>
-      <div>
-      <div className=" flex items-center justify-center w-10 h-10 mt-3 mb-3">
-        <Configuracion></Configuracion>
-      </div>
-      </div>
+          <div
+            onClick={handleClick}
+            className={`flex items-center justify-center w-10 h-10 mt-5 mb-5 rounded-lg hover:bg-gray-700 active:bg-gray-900`}
+          >
+            <Menu></Menu>
+          </div>
+          <div className=" flex items-center justify-center w-10 h-10 mt-3 mb-3 rounded-lg hover:bg-gray-700 active:bg-gray-900">
+            <Usuario></Usuario>
+          </div>
+          <div className=" flex items-center justify-center w-10 h-10 mt-3 mb-3 rounded-lg hover:bg-gray-700 active:bg-gray-900">
+            <Articulos></Articulos>
+          </div>
+          <div className=" flex items-center justify-center w-10 h-10 mt-3 mb-3 rounded-lg hover:bg-gray-700 active:bg-gray-900">
+            <Ventas></Ventas>
+          </div>
+          <div className=" flex items-center justify-center w-10 h-10 mt-3 mb-3 rounded-lg hover:bg-gray-700 active:bg-gray-900">
+            <Navegador></Navegador>
+          </div>
+          <div className=" flex items-center justify-center w-10 h-10 mt-3 mb-3 rounded-lg hover:bg-gray-700 active:bg-gray-900">
+            <Stock></Stock>
+          </div>
+          <div className=" flex items-center justify-center w-10 h-10 mt-3 mb-3 rounded-lg hover:bg-gray-700 active:bg-gray-900">
+            <Caja></Caja>
+          </div>
+          <div className=" flex items-center justify-center w-10 h-10 mt-3 mb-3 rounded-lg hover:bg-gray-700 active:bg-gray-900">
+            <Cuentas></Cuentas>
+          </div>
+          <div className=" flex items-center justify-center w-10 h-10 mt-3 mb-3 rounded-lg hover:bg-gray-700 active:bg-gray-900">
+            <Estadisticas></Estadisticas>
+          </div>
+        </div>
+        <div>
+          <div className=" flex items-center justify-center w-10 h-10 mt-3 mb-3 rounded-lg hover:bg-gray-700 active:bg-gray-900">
+            <Configuracion></Configuracion>
+          </div>
+        </div>
       </div>
     </div>
   );
