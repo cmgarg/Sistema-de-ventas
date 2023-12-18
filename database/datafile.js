e3b357bf185f1284188cee926e51c40bd6ada290
GONZALO <CMG_DEV@hotmail.com>
2023-12-18 18:54:13 -0300

    2321

--- database/datafile.js
+++ database/datafile.js
@@ -1,2 +1,1 @@
-{"nombre":"Milei","apellido":"eqwe","direccion":"qweqw","telefono":"eqweqw","email":"eqweqwe","dni":"qweqwe","_id":"YObNdrmxACbH1w7M"}
-{"nombre":"River","apellido":"asdaswew","direccion":"dasdaswew","telefono":"dasdasdwewsssss","email":"asdasdassdw","dni":"dasdaswewe","_id":"idPNJPlSW44L8Rfi"}
+{"nombre":"Riveraaaa","apellido":"asdaswew","direccion":"dasdaswew321321321321","telefono":"dasdasdwewsssss","email":"asdasdassdw","dni":"dasdaswewe","_id":"idPNJPlSW44L8Rfi"}
--- src/components/main/mainContent/ventas/Ventas.tsx
+++ src/components/main/mainContent/ventas/Ventas.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Buscador from "../../../buscador/Buscador";
+import AgregarCliente from "../clientes/buttons/AgregarCliente";
+import NavMain from "../../navmain/NavMain";
+
+
 
 export default function Ventas() {
   return (
-    <div className=' bg-red-400 h-full w-full'>
-        <div>Ventas</div>
+    <div className="flex flex-col flex-1">
+      <div className="flex-2 border-b-2 border-slate-100">
+        <NavMain>
+          <AgregarCliente onChangeModal={onChangeModal}></AgregarCliente>
+          <Buscador></Buscador>
+      </div>
+      <div className="flex flex-row flex-1">
+        <AsideMain isActive={false}></AsideMain>
+        <div className="flex-1 bg-slate-700 p-5 relative">
+          {activeModalForm && (
+            <AddClientresForm onChangeModal={onChangeModal}></AddClientresForm>
+          )}
+          {listaDeItems()}
+          {clienteAeditar.active && (
+            <EditarClientes clienteAeditarOff={clienteAeditarOff} />
+          )}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
